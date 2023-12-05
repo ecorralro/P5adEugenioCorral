057e3b4bdbf4a59edc3a9aa72b0b0c73b9b3a43c
ecorralro <ecorralr689@alumnos.imf.com>
2023-12-05 14:13:35 +0100

    Create menu

--- conector.py
+++ conector.py
@@ -1,6 +1,24 @@
 from ecdb_conector import Ecdb
 Conexion1 = Ecdb("miempresa")
-# Conexion1.update("clientes","cliente6",'{"nombre":"Luis","edad":"20"}')
-# Conexion1.select(input("Que coleccion:"),input("Que documento:"),"")
-# Conexion1.insert(input("Que coleccion:"),input("Que documento:"),input("Que contenido:"))
-Conexion1.delete(input("Que coleccion:"),input("Que documento:"))
+while True:
+    print("\nMenu:")
+    print("1. Update")
+    print("2. Select")
+    print("3. Insert")
+    print("4. Delete")
+    print("0. Salir")
+
+    opcion = input("Seleccione una opción (0-4): ")
+
+    if opcion == "0":
+        break
+    elif opcion == "1":
+        Conexion1.update(input("Que coleccion:"), input("Que documento:"), input("Que contenido nuevo:"))
+    elif opcion == "2":
+        Conexion1.select(input("Que coleccion:"), input("Que documento:"), "")
+    elif opcion == "3":
+        Conexion1.insert(input("Que coleccion:"), input("Que documento:"), input("Que contenido:"))
+    elif opcion == "4":
+        Conexion1.delete(input("Que coleccion:"), input("Que documento:"))
+    else:
+        print("Opción no válida. Intente nuevamente.")
